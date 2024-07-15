b1af415bba21f9fdf9696e6ab2ef272ee93070bb
Phil Marino <phil@damarinos.com>
2024-07-15 06:19:26 -0400

    remove unused function

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-def oneDigit(num):
-    match num:
-        case "I":
-            return 1
-        case "V":
-            return 5
-        case "X":
-            return 10
-        case "L":
-            return 50
-        case "C":
-            return 100
-        case "D":
-            return 500
-        case "M":
-            return 1000
-    
+# def oneDigit(num):
+#     match num:
+#         case "I":
+#             return 1
+#         case "V":
+#             return 5
+#         case "X":
+#             return 10
+#         case "L":
+#             return 50
+#         case "C":
+#             return 100
+#         case "D":
+#             return 500
+#         case "M":
+#             return 1000
     
 def intToRoman(num):
     temp = num
